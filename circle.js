d6e0ffceb55f4c86dcf2a60df0d2601b5d7c4b76
mickeyrich <airclayton@hotmail.com>
2023-11-21 17:57:25 -0500

    Lesson 1a

--- circle.js
+++ circle.js
@@ -1,12 +1,42 @@
-import { calculateCircumference } from "./circleCalc.js";
+import {
+  calculateCircumference,
+  calculateArea,
+  calculateVolume,
+} from "./circleCalc.js";
 
 document.querySelector("#submitButton").addEventListener("click", function (e) {
-  submitFunction();
+  submitCircumferenceFunction();
   e.preventDefault();
 });
 
-function submitFunction() {
-  let radius = circleForm.radius.value;
-  document.getElementById("output").innerHTML =
-    "The circumference is" + calculateCircumference(radius);
+document
+  .getElementById("submitAreaButton")
+  .addEventListener("click", function (e) {
+    submitAreaFunction();
+    e.preventDefault();
+  });
+
+document
+  .getElementById("submitVolumeButton")
+  .addEventListener("click", function (e) {
+    submitVolumeFunction();
+    e.preventDefault();
+  });
+
+function submitCircumferenceFunction() {
+  let radius = circleForm.radiusForCircumference.value;
+  document.getElementById("outputCircumference").innerHTML =
+    "The circumference is " + calculateCircumference(radius);
+}
+
+function submitAreaFunction() {
+  let radius = circleForm.radiusForArea.value;
+  document.getElementById("outputArea").innerHTML =
+    "The Area is " + calculateArea(radius);
+}
+
+function submitVolumeFunction() {
+  let radius = circleForm.radiusForVolume.value;
+  document.getElementById("outputVolume").innerHTML =
+    "The Volume is " + calculateVolume(radius);
 }
--- circleCalc.js
+++ circleCalc.js
@@ -1,3 +1,11 @@
 export function calculateCircumference(radius) {
   return Math.PI * radius * 2;
 }
+
+export function calculateArea(radius) {
+  return Math.PI * radius * radius;
+}
+
+export function calculateVolume(radius) {
+  return (4 / 3) * Math.PI * radius ** 3;
+}
